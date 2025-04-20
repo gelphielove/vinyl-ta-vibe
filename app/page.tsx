caba93a2aa97ce5460adbfef8d94f62f02d8a286
gelphielove <vigneau.ali@gmail.com>
2025-04-19 22:01:18 -0400

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-// app/page.tsx
-export default function Home() {
-  return (
-    <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <h1 style={{ fontFamily: 'sans-serif' }}>💿 Vinyl ta vibe est en place!</h1>
-    </main>
-  )
-}
