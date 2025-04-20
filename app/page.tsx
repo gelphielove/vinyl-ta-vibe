2e89f07cf25356a6dbc51d6d1de54b78befb70c9
gelphielove <vigneau.ali@gmail.com>
2025-04-19 21:40:49 -0400

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+// app/page.tsx
+export default function Home() {
+  return (
+    <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1 style={{ fontFamily: 'sans-serif' }}>💿 Vinyl ta vibe est en place!</h1>
+    </main>
+  )
+}
